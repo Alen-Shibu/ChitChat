987e2fafd6589b92d60531acd7c186e30829a918
Alen Shibu <your-email@example.com>
2026-02-18 11:49:11 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import 'dotenv/config'
+import path from 'path'
+
 import authRoutes from './routes/auth.route.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve();
 
 app.use('/api/auth',authRoutes)
+
+//make ready for deployment
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.use((_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`App is running on http://localhost:3000/`)
