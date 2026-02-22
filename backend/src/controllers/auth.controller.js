768be7cdf2f4af5d80af88b94b71b828eeb12b23
Alen Shibu <your-email@example.com>
2026-02-22 15:03:31 +0530

    signup controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,62 @@
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import {generateToken} from '../lib/utils.js'
+
 export const signup = async(req,res) => {
-    res.send("Signup Controller")
+    const {fullName,email,password} = req.body;
+    try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"})
+        }
+
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be atleast 6 characters long"})
+        }
+
+        //Check if email is in valid format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({ message: "user already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            await newUser.save()
+            generateToken(newUser._id,res)
+            res.status(201).json({
+                id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic
+            })
+        } else{
+            res.status(400).json({message:"Invalid User data"})
+        }
+
+
+    } catch (error) {
+        console.log('Error in signup controller:',error.message)
+        return res.status(500).json({message:"Internal Server Error"})
+    }
 }
 
 export const login = async(req,res) => {
-    res.send("Signup Controller")
+    res.send("login Controller")
 }
 
 export const logout = async(req,res) => {
-    res.send("Signup Controller")
+    res.send("logout Controller")
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import 'dotenv/config'
 import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
+import {connectDB} from './lib/db.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const __dirname = path.resolve();
 
+app.use(express.json())
 app.use('/api/auth',authRoutes)
 
 //make ready for deployment
@@ -20,5 +22,6 @@ if(process.env.NODE_ENV == "production"){
 }
 
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`App is running on http://localhost:3000/`)
 })
