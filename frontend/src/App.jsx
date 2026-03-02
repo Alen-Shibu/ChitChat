94a4688ec22aabd9a397b204a4633cac4eb1ac62
Alen Shibu <your-email@example.com>
2026-03-02 18:08:12 +0530

    frontend codebase setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes,Route} from 'react-router'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage'
+import { useAuthStore } from './stores/useAuthStore'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {isLoggedIn} = useAuthStore()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - NEW STYLE: overlapping translucent circles */}
+      <div className="absolute inset-0 bg-linear-to-tr from-indigo-500 to-purple-600 opacity-50" />
+      <div className="absolute top-10 left-10 w-72 h-72 bg-white rounded-full opacity-10 animate-pulse" />
+      <div className="absolute bottom-10 right-10 w-96 h-96 bg-yellow-400 rounded-full opacity-15 blur-xl" />
+      <Routes>
+        <Route path='/' element={<ChatPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </div>
   )
 }
 
